35265195c90fd4d805c185a6ccd0325368209f78
chuongcuu <dangchuong1999@outlook.com>
2020-04-07 10:51:07 +0300

    Setup MongoDB Atlas and connect to DB.

---
+++ config/connectDB.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+    try {
+        //Returns a promise, wait for the promise to resolve
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        //After resolving, print out the log
+        console.log('MongoDB connected...');
+    } catch (error) {
+        console.error(error.message);
+
+        //Exit the process with code failure
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
  const express = require('express');
+ const connectDB = require('./config/connectDB');
 
+ //Express server
  const app = express();
+
+ //Connect DB
+ connectDB();
 
  //Endpoints
  app.get('/', (req, res) => res.send('[server] API is running.'))
